5fb6c6a9f1fe6648dd5a9cc1243feeedcc92de6a
Stas <globalkingofusers@gmail.com>
2021-07-19 23:52:04 +0300

    initial commit

---
+++ src/test/kotlin/punccode/bfcrm/BfCrmApplicationTests.kt
@@ -0,0 +1,13 @@
+package punccode.bfcrm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BfCrmApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
