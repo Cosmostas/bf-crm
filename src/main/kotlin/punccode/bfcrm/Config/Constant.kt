1e3e7db12c515d71d5706c6d41211a9d950926b7
Sora Rise <wasbornunnamed@gmail.com>
2021-07-20 14:48:12 -0700

    Add constant class

---
+++ src/main/kotlin/punccode/bfcrm/Config/Constant.kt
@@ -0,0 +1,9 @@
+package punccode.bfcrm.Config
+
+object Constant{
+    const val DATA_VALIDATION_VIOLATION = "Data validation violation %s"
+    const val NAME_ALREADY_EXISTS = "A user with this name already exists: %s"
+    const val USER_DOES_NOT_EXIST = "User %d does not exist"
+    const val USER_WITH_NAME_AND_EMAIL_DOES_NOT_EXIST = "User %s %s not found"
+    const val USER_WITH_EMAIL_ALREADY_EXIST = "A user with such an email %s already exists"
+}
