4e4efeeb5ec1031b5d23ad7ba0fe562255bc01a0
Sora Rise <wasbornunnamed@gmail.com>
2021-07-20 10:47:31 -0700

    deleted lines that are not needed

--- src/main/kotlin/punccode/bfcrm/Config/Constant.kt
+++ src/main/kotlin/punccode/bfcrm/Config/Constant.kt
@@ -1,25 +1,7 @@
-<<<<<<< Updated upstream
-package punccode.bfcrm.Config
-
-object Constant {
-    const val DATA_VALIDATION_VIOLATION = "Data Validation Violation %s"
-    const val NAME_ALREADY_EXISTS = "A user with this name already exists: %s"
-    const val USER_DOES_NOT_EXIST = "User does not exist: %d"
-=======
-<<<<<<< HEAD
 package punccode.bfcrm.Config
 
 object Constant {
     const val DATA_VALIDATION_VIOLATION = "Data validation violation %s"
     const val NAME_ALREADY_EXISTS = "A user with this name already exists: %s"
     const val USER_DOES_NOT_EXIST = "User %s does not exist"
-=======
-package punccode.bfcrm.Config
-
-object Constant {
-    const val DATA_VALIDATION_VIOLATION = "Data Validation Violation %s"
-    const val NAME_ALREADY_EXISTS = "A user with this name already exists: %s"
-    const val USER_DOES_NOT_EXIST = "User does not exist: %d"
->>>>>>> Package
->>>>>>> Stashed changes
 }
