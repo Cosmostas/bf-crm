3fad796a60000aa2029cfc4ef1e8fe43c905eb50
Sora Rise <wasbornunnamed@gmail.com>
2021-07-20 10:24:12 -0700

    translated the errors and fixed the joint with id

--- src/main/kotlin/punccode/bfcrm/Config/Constant.kt
+++ src/main/kotlin/punccode/bfcrm/Config/Constant.kt
@@ -1,7 +1,7 @@
 package punccode.bfcrm.Config
 
 object Constant {
-    const val DATA_VALIDATION_VIOLATION = "Нарушение валидности данных %s"
-    const val NAME_ALREADY_EXISTS = "Пользователь с таким именем уже существует: %s"
-    const val USER_DOES_NOT_EXIST = "Пользователь не существует: %s"
+    const val DATA_VALIDATION_VIOLATION = "Data Validation Violation %s"
+    const val NAME_ALREADY_EXISTS = "A user with this name already exists: %s"
+    const val USER_DOES_NOT_EXIST = "User does not exist: %d"
 }
