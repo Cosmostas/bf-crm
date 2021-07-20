2b0c5b30c0399d694a50aeae3df589166d0daa57
Sora Rise <wasbornunnamed@gmail.com>
2021-07-20 01:31:42 -0700

    Create package 'Config' and add few constant variable

---
+++ src/main/kotlin/punccode/bfcrm/Config/Constant.kt
@@ -0,0 +1,7 @@
+package punccode.bfcrm.Config
+
+object Constant {
+    const val DATA_VALIDATION_VIOLATION = "Нарушение валидности данных %s"
+    const val NAME_ALREADY_EXISTS = "Пользователь с таким именем уже существует: %s"
+    const val USER_DOES_NOT_EXIST = "Пользователь не существует: %s"
+}
