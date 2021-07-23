435a4a95a0514c3671893e6d8dd54be37c4851e2
Sora Rise <wasbornunnamed@gmail.com>
2021-07-23 15:35:06 -0700

    Create migration and change application.properties

---
+++ src/main/resources/db.migration/V1__Init_DB.sql
@@ -0,0 +1,33 @@
+CREATE TABLE "Lead" (
+       "Id" bigserial,
+       "FirstName" character varying NOT NULL,
+       "LastName" character varying NOT NULL,
+       "MiddleName" character varying,
+       "PhoneNumber" character varying NOT NULL,
+       "Email" character varying NOT NULL,
+       "Gender" BOOLEAN NOT NULL,
+       "DateofBirth" DATE NOT NULL,
+       "Login" character varying NOT NULL,
+       "Password" character varying NOT NULL,
+       CONSTRAINT "Lead_pk" PRIMARY KEY ("Id")
+) WITH (
+      OIDS=FALSE
+);
+
+
+
+CREATE TABLE "Account" (
+      "Id" bigserial,
+      "LeadId" bigserial,
+      "SubscriptionType" bigserial,
+      CONSTRAINT "Account_pk" PRIMARY KEY ("Id")
+) WITH (
+      OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "Account" ADD CONSTRAINT "Account_fk0" FOREIGN KEY ("LeadId") REFERENCES "Lead"("Id");
+
+
