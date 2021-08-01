dd830b7ea1b7a5cfa991ed152f664aa98b2b707b
Sora Rise <wasbornunnamed@gmail.com>
2021-08-01 13:05:03 -0700

    Create Lead model

---
+++ src/main/kotlin/punccode/bfcrm/Entity/leadEntity.kt
@@ -0,0 +1,20 @@
+package punccode.bfcrm.Entity
+
+import java.util.*
+import javax.persistence.*
+
+@Entity
+data class leadEntity(
+    @Id
+    @SequenceGenerator(name = "leadSeq", sequenceName ="Lead_Id_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "leadSeq")
+    var id: Long,
+
+    var firstName: String,
+    var lastName: String,
+    var middleName: String,
+    var phoneNumber: String,
+    var email: String,
+    var gender: Boolean,
+    val dateOfBirth: Date
+)
