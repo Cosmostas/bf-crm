b95a48385ae709a972e8b1796db8bd4347f26a5b
Sora Rise <wasbornunnamed@gmail.com>
2021-08-01 13:20:24 -0700

    Small editm
    I forgot write annotation @Repository

--- src/main/kotlin/punccode/bfcrm/Repository/leadRepository.kt
+++ src/main/kotlin/punccode/bfcrm/Repository/leadRepository.kt
@@ -1,5 +1,7 @@
 package punccode.bfcrm.Repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import punccode.bfcrm.Entity.leadEntity
+@Repository
 interface leadRepository: JpaRepository<leadEntity, Long> {}
