b7d32273e03a8d7a814f91034cb62740a9f54898
Sora Rise <wasbornunnamed@gmail.com>
2021-08-07 06:03:08 -0700

    Some changes

--- src/main/kotlin/punccode/bfcrm/Entity/AccountEntity.kt
+++ src/main/kotlin/punccode/bfcrm/Entity/AccountEntity.kt
@@ -4,13 +4,13 @@ import org.hibernate.annotations.GeneratorType
 import javax.persistence.*
 
 @Entity
-data class accountEntity(
+data class AccountEntity(
     @Id
     @SequenceGenerator(name = "accountSeq", sequenceName = "Account_Id_seq")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "accountSeq")
     var id: Long,
     var subscriptionType: Integer,
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "lead_Id")
-    val lead: leadEntity?
+    val lead: leadId
 )
