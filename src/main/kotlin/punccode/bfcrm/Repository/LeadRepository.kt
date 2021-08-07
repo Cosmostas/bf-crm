ed4b463474431db59d5cae812a9c479b6451378f
Sora Rise <wasbornunnamed@gmail.com>
2021-08-07 05:47:49 -0700

    Delete unnecessary brackets

--- src/main/kotlin/punccode/bfcrm/Repository/LeadRepository.kt
+++ src/main/kotlin/punccode/bfcrm/Repository/LeadRepository.kt
@@ -4,4 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import punccode.bfcrm.Entity.leadEntity
 @Repository
-interface LeadRepository: JpaRepository<LeadEntity, Long>{}
+interface LeadRepository: JpaRepository<LeadEntity, Long>
